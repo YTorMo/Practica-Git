c63b6a868ce340457543677a5ae1c45d7ea540fb
YTorMo <ytoro-mo@student.42malaga.com>
2023-02-23 15:54:21 +0100

    funcionalidad de operaciones hechas

---
+++ operations.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ytoro-mo < ytoro-mo@student.42malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 15:45:45 by ytoro-mo          #+#    #+#             */
+/*   Updated: 2023/02/23 15:48:51 by ytoro-mo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int	op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int	op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int	op_div(int a, int b)
+{
+	return (a / b);
+}
---
+++ operations.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ytoro-mo < ytoro-mo@student.42malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 15:49:27 by ytoro-mo          #+#    #+#             */
+/*   Updated: 2023/02/23 15:51:17 by ytoro-mo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OPERATIONS_H
+# define OPERATIONS_H
+
+int	op_add(int a, int b);
+int	op_sub(int a, int b);
+int	op_mul(int a, int b);
+int	op_div(int a, int b);
+
+#endif
